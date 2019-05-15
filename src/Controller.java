905930d2e6d5ed9b9523446254b5188119912b30
Adam Jahraus <adam.jahraus@lidarservices.ca>
2019-05-15 08:52:41 -0600

    Project restructuring

---
+++ src/Controller.java
@@ -0,0 +1,281 @@
+import DataModel.AnnotationItem;
+import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Controller {
+
+    @FXML
+    Text outputText;
+    @FXML
+    Label outputFileLocationLabel;
+    @FXML
+    Label selectedAnnotationItemLabel;
+    @FXML
+    ImageView selectedItemImageView;
+    @FXML
+    TextArea userMessagesTextArea;
+    @FXML
+    Label annotationFileLabel;
+    @FXML
+    private Stage primaryStage;
+    @FXML
+    private ListView<AnnotationItem> annotationListView;
+    private Path outputFileLocation;
+
+    private Path annotationFileLocation;
+    private List<AnnotationItem> annotationItems;
+    private Image phImage;
+    private HashMap<AnnotationItem, Image> previewImageMap;
+    private Image rightLogo;
+    private Image leftLogo;
+
+    public void initialize() {
+        annotationItems = new ArrayList<>();
+
+        phImage = new Image("placeholder-image.jpg");
+
+        previewImageMap = new HashMap<>();
+
+        rightLogo = new Image("ATC_Logo_Resized.jpg");
+        leftLogo = new Image("LLC_Logo_Resized.jpg");
+
+        if (true) {
+            annotationFileLocation = Paths.get("Q:\\19-387\\TowerPhotos\\W3103\\W3103_Annotate.txt");
+            annotationFileLabel.setText(annotationFileLocation.toString());
+
+            outputFileLocation = Paths.get("Q:\\19-387\\TowerPhotos\\W3103\\Annotated2");
+            outputFileLocationLabel.setText("Output file location: " + outputFileLocation.toString());
+
+            Runnable task = () -> {
+                loadInBackgroundThread();
+            };
+
+            new Thread(task).start();
+        }
+
+        selectedItemImageView.setImage(phImage);
+        selectedItemImageView.preserveRatioProperty().setValue(true);
+        selectedItemImageView.setFitWidth(800);
+        selectedItemImageView.setCache(true);
+    }
+
+    @FXML
+    public void selectAnnotationFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Annotation File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                new FileChooser.ExtensionFilter("All Files", "*.*")
+        );
+        java.io.File selectedFile = fileChooser.showOpenDialog(primaryStage);
+
+        if (selectedFile != null)
+            annotationFileLocation = selectedFile.toPath();
+
+        Runnable task = () -> {
+            loadInBackgroundThread();
+        };
+
+        if (annotationFileLocation != null) {
+            annotationFileLabel.setText(annotationFileLocation.toString());
+            new Thread(task).start();
+        }
+    }
+
+    @FXML
+    private void loadAnnotationFile() throws IOException {
+
+        BufferedReader br = Files.newBufferedReader(annotationFileLocation);
+        String input;
+
+        Platform.runLater(() -> userMessagesTextArea.appendText("Loading annotation file...\n"));
+
+        try {
+            while ((input = br.readLine()) != null) {
+
+                String[] itemPieces = input.split("\t");
+
+                if (itemPieces.length != 7) {
+                    userMessagesTextArea.appendText("Warning: Line " + input + " does not have the required number of splits. Either a data " +
+                            "type is missing or there are too many tabs in this line.\n");
+                } else if (Files.exists(Paths.get(itemPieces[0])) == false) {
+                    userMessagesTextArea.appendText("Warning: File " + itemPieces[0] + " does not exit. Check that the annotation file contains the correct filepath.\n");
+                } else {
+                    AnnotationItem item = new AnnotationItem(itemPieces[0], itemPieces[1], itemPieces[2], itemPieces[3],
+                            itemPieces[4], itemPieces[5], itemPieces[6]);
+                    annotationItems.add(item);
+                }
+            }
+        } finally {
+            if (br != null) {
+                br.close();
+            }
+        }
+
+        Platform.runLater(() -> annotationListView.getItems().setAll(annotationItems));
+        Platform.runLater(() -> annotationListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE));
+        Platform.runLater(() -> userMessagesTextArea.appendText("Loading annotation file... Complete!\n"));
+
+    }
+
+    @FXML
+    private void selectOutputFileLocation() {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        Path selectedDirectory = directoryChooser.showDialog(primaryStage).toPath();
+
+        if (selectedDirectory == null) {
+            System.err.println("No directory selected");
+        } else {
+            outputFileLocation = selectedDirectory;
+            outputFileLocationLabel.setText("Output file location: " + outputFileLocation.toString());
+        }
+    }
+
+    @FXML
+    public void updateSelectedItemImagePreview() {
+        AnnotationItem item = (AnnotationItem) annotationListView.getSelectionModel().getSelectedItem();
+
+        Platform.runLater(() -> selectedAnnotationItemLabel.setText(item.toString()));
+
+        if (previewImageMap.containsKey(item)) {
+            Image image = previewImageMap.get(item);
+            Platform.runLater(() -> selectedItemImageView.setImage(image));
+        } else {
+            Platform.runLater(() -> selectedItemImageView.setImage(phImage));
+            Platform.runLater(() -> userMessagesTextArea.appendText("Image not yet loaded, please wait.\n"));
+        }
+    }
+
+    @FXML
+    private void loadInBackgroundThread() {
+        try {
+            loadAnnotationFile();
+            loadPreviewMap();
+        } catch (java.io.IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private Image createImageUsingSwingUtilFromItem(AnnotationItem item) {
+        BufferedImage inputImage = null;
+        try {
+            inputImage = ImageIO.read(item.getFilepath().toFile());
+        } catch (IOException e) {
+            System.err.println(e.getMessage() + " File in question: " + item.toString());
+            e.printStackTrace();
+            return phImage;
+        }
+
+        Image rightLogo = new Image("ATC_Logo_Resized.jpg");
+        Image leftLogo = new Image("LLC_Logo_Resized.jpg");
+
+        BufferedImage tmp = new BufferedImage(inputImage.getWidth(), inputImage.getHeight() + 800, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D graphics = tmp.createGraphics();
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, inputImage.getHeight(), tmp.getWidth(), 800);
+        graphics.drawImage(inputImage, 0, 0, null);
+
+        graphics.drawImage(SwingFXUtils.fromFXImage(leftLogo, null), 0, inputImage.getHeight(), null);
+        graphics.drawImage(SwingFXUtils.fromFXImage(rightLogo, null), (int) (inputImage.getWidth() - rightLogo.getWidth()), inputImage.getHeight(), null);
+
+        graphics.setColor(Color.BLACK);
+        graphics.setFont(new Font("Serif", Font.BOLD, 80));
+        FontMetrics metrics = graphics.getFontMetrics(graphics.getFont());
+
+        // This nonsense is used to actually put the text where I want it to be on the image. The drawString location is
+        // always placed at the top right corner, so if you want the text to be centered, you have to calculate width of
+        // your text yourself and subtract it from the location it's being printed at.
+
+        // Centered on the top row
+        String line1 = "Line: " + item.getCircuitName() + "        " + "Structure: " + item.getStructureName();
+        graphics.drawString(line1, tmp.getWidth() / 2 - (metrics.stringWidth(line1) / 2), inputImage.getHeight() + 200);
+
+        // Centered on the middle row
+        String line2 = "E: " + item.getEasting() + "        " + "N: " + item.getNorthing();
+        graphics.drawString(line2, tmp.getWidth() / 2 - (metrics.stringWidth(line2) / 2), inputImage.getHeight() + 400);
+
+        // Centered on the bottom row
+        graphics.drawString(item.getCoordinateSystem(), tmp.getWidth() / 2 - (metrics.stringWidth(item.getCoordinateSystem()) / 2), inputImage.getHeight() + 600);
+
+        // Aligned to the right edge, side height at the bottom row
+        graphics.drawString(item.getDateString(), tmp.getWidth() - (metrics.stringWidth(item.getDateString()) + 100), inputImage.getHeight() + 600);
+
+        return SwingFXUtils.toFXImage(tmp, null);
+
+    }
+
+    private Image createPreviewImageUsingSwingUtilFromItem(AnnotationItem item) {
+        Image image = createImageUsingSwingUtilFromItem(item);
+        java.awt.Image tmp = SwingFXUtils.fromFXImage(image, null).getScaledInstance((int) (image.getWidth() * (800 / image.getHeight())), 800, java.awt.Image.SCALE_SMOOTH);
+
+        BufferedImage dimg = new BufferedImage((int) (image.getWidth() * (800 / image.getHeight())), 800, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D graphics = dimg.createGraphics();
+        graphics.drawImage(tmp, 0, 0, null);
+        graphics.dispose();
+
+        return SwingFXUtils.toFXImage(dimg, null);
+    }
+
+    private Image createImageFromItem(AnnotationItem item) {
+        Image inputImage = new Image(item.getFilepath().toUri().toString());
+
+        if (inputImage == null)
+            return phImage;
+
+        Canvas canvas = new Canvas(inputImage.getWidth(), inputImage.getHeight() + 800);
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.drawImage(inputImage, 0, 0);
+        gc.drawImage(leftLogo, 0, inputImage.getHeight());
+        gc.drawImage(rightLogo, inputImage.getWidth() - rightLogo.getWidth(), inputImage.getHeight());
+
+        return canvas.snapshot(null, null);
+    }
+
+    private Image createPreviewImageFromItem(AnnotationItem item) {
+        Image image = createImageFromItem(item);
+
+        Canvas canvas = new Canvas(800 / image.getHeight() * image.getWidth(), 800);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.drawImage(image, 0, 0, 800 / image.getHeight() * image.getWidth(), 800);
+
+        return canvas.snapshot(null, null);
+    }
+
+
+    private void loadPreviewMap() {
+        Platform.runLater(() -> userMessagesTextArea.appendText("Loading preview images...\n"));
+        for (AnnotationItem item : annotationItems) {
+            previewImageMap.put(item, createPreviewImageUsingSwingUtilFromItem(item));
+        }
+        Platform.runLater(() -> userMessagesTextArea.appendText("Loading preview images... Complete!\n"));
+    }
+}
---
+++ src/DataModel/AnnotationItem.java
@@ -0,0 +1,81 @@
+package DataModel;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.format.DateTimeFormatter;
+
+public class AnnotationItem {
+    private Path filepath;
+    private String circuitName;
+    private String structureName;
+    private String easting;
+    private String northing;
+    private String coordinateSystem;
+    private String dateString; // This date string is to accommodate loading and storing the date format specifically
+    // from the annotation file. Soon this program will bypass the need for an annotation file, in which case I'll be
+    // able to use a Date object and allow the user to specify date format in this program.
+    private DateTimeFormatter formatter;
+
+    public AnnotationItem(String filepath, String circuitName, String structureName, String easting, String northing, String coordinateSystem, String dateString) {
+        this.filepath = Paths.get(filepath);
+        this.circuitName = circuitName.replace('_', ' ');
+        this.structureName = structureName.replace('_', ' ');
+        this.easting = easting.replace('_', ' ');
+        this.northing = northing.replace('_', ' ');
+        this.coordinateSystem = coordinateSystem.replace('_', ' ');
+        this.dateString = dateString.replace('_', ' ');
+    }
+
+    public Path getFilepath() {
+        return filepath;
+    }
+
+    public String getCircuitName() {
+        return circuitName;
+    }
+
+    public String getStructureName() {
+        return structureName;
+    }
+
+    public String getEasting() {
+        return easting;
+    }
+
+    public String getNorthing() {
+        return northing;
+    }
+
+    public String getCoordinateSystem() {
+        return coordinateSystem;
+    }
+
+    public String getDateString() {
+        return dateString;
+    }
+
+    @Override
+    public String toString() {
+        return filepath.toString() + '\t'
+                + circuitName + '\t'
+                + structureName + '\t'
+                + easting + '\t'
+                + northing + '\t'
+                + coordinateSystem + '\t'
+                + dateString + '\t';
+    }
+
+    public BufferedImage getBufferedImageFromFilepath() {
+        BufferedImage bufferedImage = null;
+        try {
+            bufferedImage = ImageIO.read(filepath.toFile());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return bufferedImage;
+    }
+
+}
